f7897638b334cf25ea8ccf1a733687749198c8d4
Nabeel Ahmed <nabeelahmed1699@gmail.com>
2022-06-29 22:15:32 +0500

    First commit

---
+++ task-3/js/script.js
@@ -0,0 +1,11 @@
+const collapsible = document.querySelectorAll('.collapsible')
+collapsible.forEach((item) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('collapsible--expanded')
+  })
+
+});
+// a function to sum of two numbers
+function sum(a, b) {
+  
+}
